aa3d501d7189713d4e10bc86e291f13d414d3fd3
ShrewdStylegit config --global user.name ShrewdStylegit config --global user.name ShrewdStyle <jake.webtech@gmail.com>
2020-04-03 08:32:59 +0100

    updated text info and further image compression

--- src/components/PageSetup/Work/Project-2/ProjectTwo.js
+++ src/components/PageSetup/Work/Project-2/ProjectTwo.js
@@ -19,8 +19,8 @@ const ProjectTwo = () => {
       <div className={classes.ProjectInfo}>
         <div className={classes.ProjectName}>TimeManagement</div>
         <div className={classes.ProjectText}>
-          A time management web application to help monitor and analyse your
-          daily activities to improve productivity.
+          A full stack time management web application to help monitor and
+          analyse your daily activities to improve productivity.
         </div>
         <div className={classes.ProjectPoints}>
           <ul className={classes.ProjectList}>
